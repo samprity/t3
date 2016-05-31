c6da59ce51d1d5356bbf06b8a5c660d8393a808d
Piyush <piyushkumar1523@gmail.com>
2016-05-30 21:52:59 -0700

    Adding hierarchy to task model

--- api/get.js
+++ api/get.js
@@ -3,9 +3,16 @@
 var Task = require('../models/task.js').Task();
 
 module.exports.GetTasks = function(){
-    var tasks = [];
-    for(var i=0;i<3;i++){
-        tasks.push(new Task(i,false));
-    }
+    var tasks = [
+        new Task(0,false),
+        new Task(1,true, [
+            new Task(3,false)
+        ])
+    ];
+
+    tasks[0].AddTask(new Task(2,true));
+    tasks[0].AddTask(new Task(5,true));
+    tasks[1].SubTasks[0].AddTask(new Task(4,true));
+
     return tasks;
 }
--- models/task.js
+++ models/task.js
@@ -3,9 +3,16 @@
 // The class Task
 // id : identifier of the task
 // done : true iff the task has been completed
-var Task = function(id, done) {
-    this.id=id;
-    this.done=done;
+// children : a list of Task which are children of the Task being created
+
+var Task = function(id, done, children) {
+    this.Id = id;
+    this.Done = done;
+    this.SubTasks = children===undefined ? [] : children;
+
+    this.AddTask = function(task) {
+        this.SubTasks.push(task);
+    }
 };
 
 module.exports.Task = function(){
